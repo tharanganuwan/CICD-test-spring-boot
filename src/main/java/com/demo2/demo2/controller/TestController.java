b7d37be92a466bf886c0a1e33c359af3fb4a5eaa
Tharanga Nuwan <rocketnuwan.tharanga99@gmail.com>
2023-06-27 10:22:25 +0530

    initial commit

---
+++ src/main/java/com/demo2/demo2/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.demo2.demo2.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/student")
+public class TestController{
+    
+    @GetMapping("/getAll")
+    public String getAllStudent(){
+        return "Tharanga nuwan";
+    }
+
+}
